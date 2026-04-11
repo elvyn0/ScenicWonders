f181d18a6682483b4c0c21d27453335d0d9c2b8f
Elvyn <122730895+elvyn0@users.noreply.github.com>
2026-04-12 02:04:16 +0530

    fix:minor pading issue

--- frontend/src/Pages/PostById.jsx
+++ frontend/src/Pages/PostById.jsx
@@ -103,7 +103,7 @@ function PostById() {
     );
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center py-4">
       {/* User Posts */}
       {post && (
         <div
